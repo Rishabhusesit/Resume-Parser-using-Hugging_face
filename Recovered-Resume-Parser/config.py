23a8a0021683e24bc548712daad53a1b8a93e9bd
rishabh <rishabh.09919011722@ipu.ac.in>
2025-03-09 22:59:36 +0530

    Test pre-commit hook

--- Recovered-Resume-Parser/config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql://rishabhtiwari@host.docker.internal:5432/recruitment_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
-
--- Recovered-Resume-Parser/models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class CandidateScore(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(100))
-    email = db.Column(db.String(100))
-    match_score = db.Column(db.Float)
